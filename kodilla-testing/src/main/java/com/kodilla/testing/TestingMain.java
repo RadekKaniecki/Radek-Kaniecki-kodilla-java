d48b533e8a2163b0d53beff1f5032047f70394a8
RadekKaniecki <radoslawkaniecki@gmail.com>
2017-08-08 22:36:13 +0200

    First unit test exercise

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Modul 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("test OK");
+        }else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy.");
+
+        Calculator calc = new Calculator();
+        int a = 10;
+        int b = 5;
+
+        int addResult = calc.add(a, b);
+        if(addResult == 15) {
+            System.out.println("Add test - OK.");
+        }else {
+            System.out.println("Error.");
+        }
+
+        int subResult = calc.subtract(a, b);
+        if(subResult == 5) {
+            System.out.println("Subtract test - OK.");
+        }else {
+            System.out.println("Error.");
+        }
     }
 }
