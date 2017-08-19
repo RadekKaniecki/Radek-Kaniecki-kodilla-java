1563081e494e00839efb5cee5973b7d85dee8fb0
RadekKaniecki <radoslawkaniecki@gmail.com>
2017-08-19 15:58:06 +0200

    Mockito 6.5

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -0,0 +1,37 @@
+package com.kodilla.testing.library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookLibrary {
+    LibraryDatabase libraryDatabase;
+
+    public BookLibrary(LibraryDatabase libraryDatabase) {
+        this.libraryDatabase = libraryDatabase;
+    }
+
+    public List<Book> listBooksWithCondition(String titleFragment) {
+        //creating empty list of books
+        List<Book> bookList = new ArrayList<Book>();
+
+        //first condition - if title fragment passed into method is shorter than 3 - return empty list
+        if (titleFragment.length() < 3){
+            return bookList;
+        }
+
+        //creating list of books with actual records from database
+        List<Book> resultList = libraryDatabase.listBooksWithCondition(titleFragment);
+
+        //second condition - if there will be more than 20 results from database providing title fragment
+        //method will return empty list
+        if (resultList.size() > 20) {
+            return bookList;
+        }
+
+        //else empty list created in a first place will be pointing to result list from database
+        //and method will finally return this list
+        bookList = resultList;
+        return bookList;
+    }
+
+}
