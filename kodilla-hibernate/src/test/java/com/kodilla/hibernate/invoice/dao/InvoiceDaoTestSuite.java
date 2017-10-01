96a47acf94244e194cd5463c43a9385c92bd4a16
RadekKaniecki <radoslawkaniecki@gmail.com>
2017-10-01 18:43:28 +0200

    Exercise 13.3

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -36,15 +36,19 @@ public class InvoiceDaoTestSuite {
 
         Invoice invoice = new Invoice("12345");
 
-        product1.getItems().add(item1);
-        product1.getItems().add(item2);
-        product2.getItems().add(item3);
+        item1.setProduct(product1);
+        item2.setProduct(product1);
+        item3.setProduct(product2);
 
-        invoice.getItems().add(item1);
-        invoice.getItems().add(item2);
-        invoice.getItems().add(item3);
+        item1.setInvoice(invoice);
+        item2.setInvoice(invoice);
+        item3.setInvoice(invoice);
 
         //When
+        productDao.save(product1);
+        productDao.save(product2);
+        int productId = product2.getId();
+
         invoiceDao.save(invoice);
         int invoiceId = invoice.getId();
 
@@ -53,18 +57,14 @@ public class InvoiceDaoTestSuite {
         itemDao.save(item3);
         int itemId = item3.getId();
 
-        productDao.save(product1);
-        productDao.save(product2);
-        int productId = product2.getId();
-
         //Then
         Assert.assertEquals(1, invoiceId);
         Assert.assertEquals(3, itemId);
         Assert.assertEquals(2, productId);
 
         //CleanUp
-        productDao.deleteAll();
-        itemDao.deleteAll();
-        invoiceDao.deleteAll();
+//        productDao.deleteAll();
+//        itemDao.deleteAll();
+//        invoiceDao.deleteAll();
     }
 }
